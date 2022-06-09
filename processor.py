62d8538a9d99849469dcab013de126338ddebb98
Muntazir Qasim <muntazirqasim@gmail.com>
2022-06-09 11:32:20 +0100

    feat: Rewrote code into functions to be modular

--- processor.py
+++ processor.py
@@ -1,32 +1,29 @@
-from os import kill
 import pandas as pd
 import numpy as np
 from datetime import timedelta
-from pyspark.sql.functions import *
 
 # reads CSV file worker_activity.csv in the same directory as the script and puts it in a DataFrame
 # parse_dates=False as dates are managed as numpy datetime during processing
-def read_csv():
-    final_df = pd.DataFrame(columns=['Worker','Continuity'])
-    df = pd.read_csv('worker_activity.csv', parse_dates=True)#, index_col=0)
+def read_csv(csv_file):
+    df = pd.read_csv(csv_file, parse_dates=True)#, index_col=0)
     # sort df to order by Worker
     df = df.sort_values('Worker')
-    # change Date format from string to datetime
-    #df['Date'] = np.datetime64(df['Date'])
-    #df['Date'].astype('timedelta64[D]')
-    #df['Date'] = pd.to_datetime(df['Date'])
-    worker_list = set(df['Worker'])
-    #print(df['Date'])
 
-    #df = df.tail()
-    #print(df)
+    return df
+
+def process_data(df):
+
+    # create list of unique workers to be used for processing when iterating
+    worker_list = set(df['Worker'])
+
+    # creating empty dataframe to be populated with final data of workers and continuity to be written into csv
+    final_df = pd.DataFrame(columns=['Worker','Continuity'])
 
     # retrieve ordered list of all dates for each record per worker
     for worker in worker_list:
         # creating a dataframe for each individual worker in the for loop
         worker_df = df[df.Worker == worker]
-        #print(df.head)
-        #print(worker_df)
+
         # define list of all the dates
         worker_dates = worker_df['Date'].values
         # order list
@@ -51,9 +48,6 @@ def read_csv():
             six_days = np.datetime64(6, 'D') - np.datetime64(0,'Y')
             six_days = six_days.astype('timedelta64[D]')
 
-            seven_days = np.datetime64(7, 'D') - np.datetime64(0,'Y')
-            seven_days = seven_days.astype('timedelta64[D]')
-
             prev_employer = 0
             prev_role = 0
 
@@ -61,6 +55,7 @@ def read_csv():
             current_role = employer_and_role_df['Role'].values
             current_employer = employer_and_role_df['Employer'].values
 
+            # if the two dates being compared are within a six day time period, the number of days worked is incremented
             # if the role or employer of the previous working day and current day aren't equal, activity length is set to
             # greater than six days so that the days_worked isn't incremented in the next step 
             if int(prev_role) == 0 and int(prev_employer) == 0:
@@ -70,23 +65,33 @@ def read_csv():
             elif int(current_role[0]) == int(prev_role) and int(current_employer[0]) == int(prev_employer):
                 if activity_length <= six_days:
                     days_worked += 1
-
+            # if role or employer for current date of employment isn't the same as it was for previous date of employment
+            # the loop breaks and the continuity stops being incremented for this worker
             else:
-                activity_length = seven_days
                 break
-            # if the two dates being compared are within a six day time period, the number of days worked is incremented
 
-                ### need this to verify whether this breaks out of the for loop so that it doesn't process 
-                ### any more dates for the worker
+            # setting prev variables in preparation for the next loop
             prev_date = date
             prev_employer = current_employer
             prev_role = current_role
-            #print(days_worked)
-            
+
+        # appending the worker and continuity row to the final dataframe for this worker    
         final_df = final_df.append({'Worker': worker,'Continuity': days_worked}, ignore_index=True)
 
+    return final_df
+
+def write_csv(final_df):
+    # sort dataframe by descending continuity
     final_df = final_df.sort_values(by=['Continuity'], ascending=False)
+    print(final_df)
 
-    final_df.to_csv('results.csv', index=False)
+    # write final dataframe as a csv called results.csv
+    #final_df.to_csv('results.csv', index=False)
 
-read_csv()
+def main():
+    df = read_csv('worker_activity.csv')
+    final_df = process_data(df)
+    write_csv(final_df)
+
+if __name__ == "__main__":
+    main()
